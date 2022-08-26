8674b167574a24ab437d889843c7b5fa2b62773f
emmegiesse <martagiulia.scoditti@gmail.com>
2022-08-26 03:07:51 +0200

    index.js

--- kasa/src/index.js
+++ kasa/src/index.js
@@ -3,15 +3,31 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import Header from './components/header';
+import Footer from './components/footer';
+import "./style/index.css";
+import {BrowserRouter as Router,Route,Routes} from "react-router-dom";
+import Home from './pages/home';
+import Error from './pages/error';
+import House from './pages/house';
+import About from "./pages/about";
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+    <Router basename={process.env.PUBLIC_URL}>
+      <main className='design'>
+        <Header />
+          <Routes>
+            <Route path="/" element={<Home/>} />
+            <Route path="about" element={<About/>} />
+            <Route path="*" element={<Error/>} />
+            <Route path="housing/:id/*" element={<House/>} />
+          </Routes>
+      </main>
+      <Footer />   
+    </Router>
+  </React.StrictMode>,
+  document.getElementById('root')
+  );
+  
