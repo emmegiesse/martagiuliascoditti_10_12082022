d7d07a3ea73b7cbe001d1a8853584bd9ddbc3b95
emmegiesse <martagiulia.scoditti@gmail.com>
2022-08-26 17:42:46 +0200

    root fix

--- kasa/src/index.js
+++ kasa/src/index.js
@@ -5,26 +5,25 @@ import Header from './components/header';
 import Footer from './components/footer';
 import "./style/index.css";
 import {BrowserRouter as Router,Route,Routes} from "react-router-dom";
-import Home from './pages/home';
+import HomePage from './pages/homepage';
 import Error from './pages/error';
-import House from './pages/house';
+import Property from './pages/property';
 import About from "./pages/about";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-  <Router basename={process.env.PUBLIC_URL}>
-  <main className='design'>
-  <Header />
-  <Routes>
-  <Route path="/" element={<Home/>} />
-  <Route path="about" element={<About/>} />
-  <Route path="*" element={<Error/>} />
-  <Route path="housing/:id/*" element={<House/>} />
-  </Routes>
-  </main>
-  <Footer />   
-  </Router>
-  </React.StrictMode>,
-  document.getElementById('root')
+    <Router basename={process.env.PUBLIC_URL}>
+      <main className="design">
+        <Header />
+        <Routes>
+            <Route path="/" element={<HomePage/>} />
+            <Route path="about" element={<About/>} />
+            <Route path="housing/:id/*" element={<Property/>} />
+            <Route path="*" element={<Error/>} />
+        </Routes>
+      </main>
+      <Footer />   
+    </Router>
+  </React.StrictMode>
 );
